dff0ae36b6afd39a6cd7399a658e2f43b9f11c9d
kento <kozukata1993@gmail.com>
2020-04-25 19:13:40 +0900

    ログイン機能を実装 Google, Facebook, Twitterでログイン

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
+import { authentication } from "./firebase/index";
+import { Home } from "./components/home";
 import { LoginForm } from "./components/loginForm";
+import { User } from "./interfaces";
 
 const App: FC = () => {
-  return (
-    <div className="App">
-      <LoginForm />
-    </div>
-  );
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  authentication().onAuthStateChanged((user) => {
+    setCurrentUser(user);
+  });
+
+  if (currentUser) {
+    return (
+      <>
+        <Home />
+      </>
+    );
+  }
+
+  return <LoginForm />;
 };
 
 export default App;
--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,44 +1,90 @@
-import React, { FC } from "react";
+import React, { FC, useState, FormEvent } from "react";
 import { Button, Form, Grid, Header, Message, Segment, Icon } from "semantic-ui-react";
+import { authentication } from "../firebase/index";
+import { login, signUp, createUser, loginWithSocialAccount } from "../firebase/auth";
+import { InputData } from "../interfaces";
 
-export const LoginForm: FC = () => (
-  <Grid textAlign="center" style={{ height: "90vh" }} verticalAlign="middle">
-    <Grid.Column style={{ maxWidth: 500 }}>
-      <Header as="h1" color="blue" textAlign="center">
-        Login
-      </Header>
-      <Form size="large">
-        <Segment basic>
-          <Form.Field required>
-            <Form.Input fluid icon="at" iconPosition="left" placeholder="E-mail address" />
-            <Form.Input
-              fluid
-              icon="lock"
-              iconPosition="left"
-              placeholder="Password"
-              type="password"
-            />
+if (authentication().isSignInWithEmailLink(window.location.href)) {
+  createUser();
+}
 
-            <Button color="blue" fluid size="large">
-              Login
-            </Button>
-          </Form.Field>
-        </Segment>
-        <Button color="facebook">
-          <Icon name="facebook" /> Facebook
-        </Button>
-        <Button color="twitter">
-          <Icon name="twitter" /> Twitter
-        </Button>
-        <Button color="google plus">
-          <Icon name="google" /> Google
-        </Button>
-        <Button color="instagram">
-          <Icon name="instagram" /> Instagram
-        </Button>
-      </Form>
+export const LoginForm: FC = () => {
+  const [email, setEmail] = useState<string | null>(null);
+  const [password, setPassword] = useState<string | null>(null);
 
-      <Message>New to us? Sign Up</Message>
-    </Grid.Column>
-  </Grid>
-);
+  const handleClick = async () => {
+    if (email && password) {
+      const methods = await authentication().fetchSignInMethodsForEmail(email);
+      if (methods[0]) {
+        login(email, password);
+      } else {
+        signUp(email, password);
+      }
+    }
+  };
+
+  const handleChangeEmail = (event: FormEvent, { value }: InputData) => {
+    setEmail(value);
+  };
+
+  const handleChangePassword = (event: FormEvent, { value }: InputData) => {
+    setPassword(value);
+  };
+
+  const loginWithFacebook = () => {
+    loginWithSocialAccount(new authentication.FacebookAuthProvider());
+  };
+
+  const loginWithTwitter = () => {
+    loginWithSocialAccount(new authentication.TwitterAuthProvider());
+  };
+
+  const loginWithGoogle = () => {
+    loginWithSocialAccount(new authentication.GoogleAuthProvider());
+  };
+
+  return (
+    <Grid textAlign="center" style={{ height: "90vh" }} verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 500 }}>
+        <Header as="h1" color="blue" textAlign="center" content="Log in" />
+        <Form size="large">
+          <Segment basic>
+            <Form.Field required>
+              <Form.Input
+                fluid
+                onChange={handleChangeEmail}
+                icon="at"
+                iconPosition="left"
+                placeholder="E-mail address"
+              />
+              <Form.Input
+                fluid
+                onChange={handleChangePassword}
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                type="password"
+              />
+              <Form.Button color="blue" fluid size="large" onClick={handleClick} content="Log in" />
+            </Form.Field>
+          </Segment>
+          <Header as="h3">Log in with social accounts</Header>
+          <Button color="google plus" onClick={loginWithGoogle}>
+            <Icon name="google" />
+            Google
+          </Button>
+          <Button color="facebook" onClick={loginWithFacebook}>
+            <Icon name="facebook" />
+            Facebook
+          </Button>
+          <Button color="twitter" onClick={loginWithTwitter}>
+            <Icon name="twitter" />
+            Twitter
+          </Button>
+        </Form>
+
+        <Message>New to us? Sign Up</Message>
+      </Grid.Column>
+    </Grid>
+  );
+};
