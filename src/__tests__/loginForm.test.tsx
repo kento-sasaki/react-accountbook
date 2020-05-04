91ece17a16840746e97df9574cec786e148e1837
kento <kozukata1993@gmail.com>
2020-05-04 16:52:21 +0900

    カウンターのテストを実装 user-eventをインストール user.typeを試す

---
+++ src/__tests__/loginForm.test.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import userEvent from "@testing-library/user-event";
+import { render, cleanup, fireEvent } from "../utils/test-utils";
+import { LoginForm } from "../components/loginForm";
+import "@testing-library/jest-dom";
+
+afterEach(cleanup);
+
+describe("LoginForm", () => {
+  it.todo("メールアドレスは必須");
+  it.todo("パスワードは必須");
+  it.todo("");
+  it.todo("ログインしたらcurrentUserが更新される", async () => {
+    const { getByPlaceholderText, getByTestId } = render(<LoginForm />);
+    await userEvent.type(getByPlaceholderText("E-mail address"), "username@example.com");
+  });
+});
