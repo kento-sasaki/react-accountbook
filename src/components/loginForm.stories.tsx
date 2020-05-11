7d1952237ea40f95ace4e0083935ac8fde0690a6
kento <kozukata1993@gmail.com>
2020-05-11 22:37:53 +0900

    containerとpresentationalに分離

--- src/components/loginForm.stories.tsx
+++ src/components/loginForm.stories.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import "semantic-ui-css/semantic.min.css";
 import { Provider } from "react-redux";
-import { LoginForm } from "./loginForm";
+import { LoginFormComponent } from "./loginForm";
 import { store } from "../stores/index";
 
 export default {
-  component: LoginForm,
+  component: LoginFormComponent,
   title: "LoginForm",
 };
 
 export const loginForm = () => {
   return (
     <Provider store={store}>
-      <LoginForm />
+      <LoginFormComponent />
     </Provider>
   );
 };
