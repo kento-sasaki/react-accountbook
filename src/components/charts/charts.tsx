6e4eaeacef171b0047eecca82b97fa8f517d2b11
kento <kozukata1993@gmail.com>
2020-06-16 22:18:29 +0900

    addExpenseFormとaddIncomeFormのボタンを修正

--- src/components/charts/charts.tsx
+++ src/components/charts/charts.tsx
@@ -1,7 +1,16 @@
 /** @jsx jsx */
 import React, { FC } from 'react';
 import { jsx } from '@emotion/core';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, ResponsiveContainer } from 'recharts';
+import {
+  LineChart,
+  Line,
+  BarChart,
+  Bar,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+} from 'recharts';
 import { createDatilyExpense } from '../../firebase/firestore';
 import { Expense } from '../../interfaces';
 
@@ -20,13 +29,23 @@ export const ExpenseChart: FC<ExpenseChartProps> = ({ expense }) => {
   });
 
   return (
-    <ResponsiveContainer width="100%" height={400}>
-      <LineChart data={data} margin={{ top: 20, right: 20, bottom: 5, left: 0 }}>
-        <Line type="monotone" dataKey="amount" stroke="#8884d8" strokeWidth={2} />
-        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-        <XAxis dataKey="formatedDate" />
-        <YAxis />
-      </LineChart>
-    </ResponsiveContainer>
+    <div>
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart data={data} margin={{ top: 20, right: 20, bottom: 5, left: 0 }}>
+          <Line type="monotone" dataKey="amount" stroke="#8884d8" strokeWidth={2} />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="formatedDate" />
+          <YAxis />
+        </LineChart>
+      </ResponsiveContainer>
+      <ResponsiveContainer>
+        <BarChart data={data} margin={{ top: 20, right: 20, bottom: 5, left: 0 }}>
+          <Bar dataKey="amount" stroke="#8884d8" strokeWidth={2} />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="formatedDate" />
+          <YAxis />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
   );
 };
