84b80a59dee7b521426be57a15c44fc837c6a906
kento <kozukata1993@gmail.com>
2020-06-16 22:34:48 +0900

    BarChartに変更

--- src/components/charts/charts.tsx
+++ src/components/charts/charts.tsx
@@ -1,16 +1,7 @@
 /** @jsx jsx */
 import React, { FC } from 'react';
 import { jsx } from '@emotion/core';
-import {
-  LineChart,
-  Line,
-  BarChart,
-  Bar,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  ResponsiveContainer,
-} from 'recharts';
+import { BarChart, Bar, CartesianGrid, XAxis, YAxis, ResponsiveContainer } from 'recharts';
 import { createDatilyExpense } from '../../firebase/firestore';
 import { Expense } from '../../interfaces';
 
@@ -31,16 +22,8 @@ export const ExpenseChart: FC<ExpenseChartProps> = ({ expense }) => {
   return (
     <div>
       <ResponsiveContainer width="100%" height={400}>
-        <LineChart data={data} margin={{ top: 20, right: 20, bottom: 5, left: 0 }}>
-          <Line type="monotone" dataKey="amount" stroke="#8884d8" strokeWidth={2} />
-          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-          <XAxis dataKey="formatedDate" />
-          <YAxis />
-        </LineChart>
-      </ResponsiveContainer>
-      <ResponsiveContainer>
         <BarChart data={data} margin={{ top: 20, right: 20, bottom: 5, left: 0 }}>
-          <Bar dataKey="amount" stroke="#8884d8" strokeWidth={2} />
+          <Bar dataKey="amount" fill="#8884d8" />
           <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
           <XAxis dataKey="formatedDate" />
           <YAxis />
