5ec252845e1b3c69c35f4ad049db851d84a27c14
kento <kozukata1993@gmail.com>
2020-07-13 16:30:53 +0900

    ファイル選択ボタンを作成 ファイル登録ボタンを作成 長過ぎるファイル名は短く表示 snapshotsを更新

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,93 +3,93 @@ module.exports = {
     browser: true,
     es6: true,
     node: true,
-    "jest/globals": true,
+    'jest/globals': true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:jest/recommended",
-    "airbnb",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "prettier",
-    "prettier/@typescript-eslint",
-    "prettier/react",
-    "prettier/standard",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:jest/recommended',
+    'airbnb',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+    'prettier/standard',
   ],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-    cy: "readonly",
-    Cypress: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    cy: 'readonly',
+    Cypress: 'readonly',
     __DEV__: true,
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    project: ["./tsconfig.json", "./cypress/tsconfig.json"],
-    sourceType: "module",
+    project: ['./tsconfig.json', './cypress/tsconfig.json'],
+    sourceType: 'module',
   },
   plugins: [
-    "@typescript-eslint",
-    "import",
-    "jest",
-    "jsx-a11y",
-    "prefer-arrow",
-    "emotion",
-    "prettier",
-    "react",
-    "react-hooks",
+    '@typescript-eslint',
+    'import',
+    'jest',
+    'jsx-a11y',
+    'prefer-arrow',
+    'emotion',
+    'prettier',
+    'react',
+    'react-hooks',
   ],
   root: true,
   rules: {
     // eslint official
-    "newline-before-return": "error",
-    "no-console": "warn",
-    "no-continue": "off",
-    "require-yield": "error",
+    'newline-before-return': 'error',
+    'no-console': 'warn',
+    'no-continue': 'off',
+    'require-yield': 'error',
     // for react-app-env.d.ts (https://github.com/facebook/create-react-app/issues/6560)
-    "spaced-comment": [
-      "error",
-      "always",
+    'spaced-comment': [
+      'error',
+      'always',
       {
-        markers: ["/"],
+        markers: ['/'],
       },
     ],
 
     // for Cypress
-    "jest/expect-expect": ["off"],
+    'jest/expect-expect': ['off'],
 
     // for Emotion
-    "emotion/jsx-import": "error",
-    "emotion/no-vanilla": "error",
-    "emotion/import-from-emotion": "error",
-    "emotion/styled-import": "error",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
+    'emotion/jsx-import': 'error',
+    'emotion/no-vanilla': 'error',
+    'emotion/import-from-emotion': 'error',
+    'emotion/styled-import': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
       {
-        varsIgnorePattern: "[Rr]eact",
+        varsIgnorePattern: '[Rr]eact',
       },
     ],
 
     // @typescript-eslint
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-member-accessibility": "off",
-    indent: "off",
-    "@typescript-eslint/indent": "off",
-    "@typescript-eslint/no-unnecessary-type-assertion": "error",
-    "@typescript-eslint/no-empty-function": "off",
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    indent: 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    '@typescript-eslint/no-empty-function': 'off',
 
     // prefer-arrow
-    "prefer-arrow/prefer-arrow-functions": [
-      "error",
+    'prefer-arrow/prefer-arrow-functions': [
+      'error',
       {
         disallowPrototype: true,
         singleReturnOnly: true,
@@ -98,63 +98,73 @@ module.exports = {
     ],
 
     // react
-    "react/jsx-filename-extension": [
-      "error",
+    'react/jsx-filename-extension': [
+      'error',
       {
-        extensions: ["jsx", "tsx"],
+        extensions: ['jsx', 'tsx'],
       },
     ],
-    "react/jsx-props-no-spreading": [
-      "warn",
+    'react/jsx-props-no-spreading': [
+      'warn',
       {
-        custom: "ignore",
+        custom: 'ignore',
       },
     ],
-    "react/prop-types": "off",
-    "react/prefer-stateless-function": "off",
+    'react/prop-types': 'off',
+    'react/prefer-stateless-function': 'off',
 
     // react hooks
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "error",
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
 
     // import
-    "import/extensions": [
-      "error",
-      "always",
+    'import/extensions': [
+      'error',
+      'always',
       {
-        js: "never",
-        jsx: "never",
-        ts: "never",
-        tsx: "never",
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
       },
     ],
-    "import/prefer-default-export": "off",
-    "import/no-extraneous-dependencies": [
-      "error",
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
       {
         devDependencies: [
-          ".storybook/**",
-          "stories/**",
-          "**/*/*.story.*",
-          "**/*/*.stories.*",
-          "**/__tests__/**",
-          "**/*/*.test.*",
+          '.storybook/**',
+          'stories/**',
+          '**/*/*.story.*',
+          '**/*/*.stories.*',
+          '**/__tests__/**',
+          '**/*/*.test.*',
         ],
+      },
+    ],
+
+    // 自分ルール
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id'],
+        },
       },
     ],
   },
   settings: {
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        extensions: [".js", "jsx", ".ts", ".tsx"],
-        paths: ["src"],
+        extensions: ['.js', 'jsx', '.ts', '.tsx'],
+        paths: ['src'],
       },
     },
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
 };
--- src/containers/inputFile.tsx
+++ src/containers/inputFile.tsx
@@ -1,17 +1,39 @@
 /** @jsx jsx */
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import { jsx } from '@emotion/core';
 import { uploadFile } from '../firebase/storage';
 import { InputFileComponent } from '../components/inputFile/inputputFile';
 
 export const InputFile: FC = () => {
-  const id = 'inputFile';
+  const [fileData, setFileData] = useState<File | undefined>();
+  const [displayName, setDisplayName] = useState<string>('File name');
 
-  const handleFile = async (e: any) => {
-    const file: File = e.target.files[0];
-    console.log(file);
-    await uploadFile(file);
+  useEffect(() => {
+    if (fileData) {
+      const tempDisplayName =
+        fileData.name.length > 25
+          ? `${fileData.name.substr(0, 10)}...${fileData.name.substr(-10)}`
+          : fileData.name;
+      setDisplayName(tempDisplayName);
+    }
+  }, [displayName, fileData]);
+
+  const selectFile = async (e: any) => {
+    setFileData(e.target.files[0]);
+    // await uploadFile(file);
   };
 
-  return <InputFileComponent id={id} handleFile={handleFile} />;
+  const handleUploadClick = () => {
+    if (fileData) {
+      uploadFile(fileData);
+    }
+  };
+
+  return (
+    <InputFileComponent
+      selectFile={selectFile}
+      file={{ data: fileData, displayName }}
+      handleUploadClick={handleUploadClick}
+    />
+  );
 };
