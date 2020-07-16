86e81bf994acdea794dd2814ae356bbd358925c3
kento <kozukata1993@gmail.com>
2020-07-17 07:45:38 +0900

    counterのsliceを削除

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
-import { counterReducer } from './counter';
 import { expenseReducer } from './expense';
 
 const reducer = combineReducers({
-  counter: counterReducer,
   expense: expenseReducer,
 });
 
