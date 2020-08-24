314de1deae1af0ac038197581227aadf99bf4c3f
kento <kozukata1993@gmail.com>
2020-08-24 21:30:56 +0900

    React.memoでレンダリングを減らす

--- src/components/charts/expenseBarChart.tsx
+++ src/components/charts/expenseBarChart.tsx
@@ -10,6 +10,29 @@ interface ExpenseBarChartProps {
   expense: StoreExpense[];
 }
 
+interface MemoChartProps {
+  data: {
+    formatedDate: string;
+    amount: number;
+  }[];
+  height: 300 | 187;
+}
+
+const MemoChart: FC<MemoChartProps> = React.memo(({ data, height }) => {
+  console.log('Render MemoChart');
+
+  return (
+    <ResponsiveContainer width="100%" height={height}>
+      <BarChart data={data} margin={{ top: 20, right: 20, bottom: 5, left: 0 }}>
+        <Bar dataKey="amount" fill="#00b5ad" />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="formatedDate" />
+        <YAxis />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+});
+
 export const ExpenseBarChart: FC<ExpenseBarChartProps> = ({ expense }) => {
   const device = useSelector((store: Store) => store.device.device);
   const height = device === 'widescreen' || device === 'largeScreen' ? 300 : 187;
@@ -23,14 +46,18 @@ export const ExpenseBarChart: FC<ExpenseBarChartProps> = ({ expense }) => {
     };
   });
 
+  console.log('Render BarChart');
+
   return (
-    <ResponsiveContainer width="100%" height={height}>
-      <BarChart data={data} margin={{ top: 20, right: 20, bottom: 5, left: 0 }}>
-        <Bar dataKey="amount" fill="#00b5ad" />
-        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-        <XAxis dataKey="formatedDate" />
-        <YAxis />
-      </BarChart>
-    </ResponsiveContainer>
+    // <ResponsiveContainer width="100%" height={height}>
+    //   <BarChart data={data} margin={{ top: 20, right: 20, bottom: 5, left: 0 }}>
+    //     {console.log('BarChart')}
+    //     <Bar dataKey="amount" fill="#00b5ad" />
+    //     <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+    //     <XAxis dataKey="formatedDate" />
+    //     <YAxis />
+    //   </BarChart>
+    // </ResponsiveContainer>
+    <MemoChart data={data} height={height} />
   );
 };
