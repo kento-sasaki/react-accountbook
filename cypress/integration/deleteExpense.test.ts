44ac7be9a6a49c919f518556e247a71fc6637295
kento <kozukata1993@gmail.com>
2020-10-21 19:20:44 +0900

    deleteExpense.test.ts をリファクタリング

--- cypress/integration/deleteExpense.test.ts
+++ cypress/integration/deleteExpense.test.ts
@@ -15,37 +15,34 @@ describe('delete expnese', () => {
   });
 
   it('支出の削除', () => {
-    const amount = Math.floor(Math.random() * 1000);
     cy.wait(3000);
-    cy.findByPlaceholderText('Expense').type(`${amount}`);
-    cy.findByRole('button', { name: 'Submit' }).click();
-    cy.wait(3000);
-    cy.findAllByTestId('table-unit').first().contains(`${amount}`);
-    cy.findAllByTestId('table-unit')
+
+    return cy
+      .findAllByTestId('table-unit')
       .first()
       .then((element) => {
-        console.log(element);
+        const amount = element[0].childNodes[1].textContent;
 
-        console.log(element[0].childNodes[1].textContent);
+        cy.findAllByTestId('table-unit').first().contains('Delete').click();
+        cy.findByRole('button', { name: 'OK' }).click();
+        cy.wait(3000);
+        cy.findAllByTestId('table-unit').first().should('not.contain', amount);
       });
-
-    cy.findAllByTestId('table-unit').first().contains('Delete').click();
-    cy.findByRole('button', { name: 'OK' }).click();
-    cy.wait(3000);
-    cy.findAllByTestId('table-unit').first().should('not.contain', amount);
   });
 
   it('支出の削除をキャンセル', () => {
-    const amount = Math.floor(Math.random() * 1000);
     cy.wait(3000);
-    cy.findByPlaceholderText('Expense').type(`${amount}`);
-    cy.findByRole('button', { name: 'Submit' }).click();
-    cy.wait(3000);
-    cy.findAllByTestId('table-unit').first().contains(`${amount}`);
 
-    cy.findAllByTestId('table-unit').first().contains('Delete').click();
-    cy.findByRole('button', { name: 'Cancel' }).click();
-    cy.wait(3000);
-    cy.findAllByTestId('table-unit').first().contains(`${amount}`);
+    return cy
+      .findAllByTestId('table-unit')
+      .first()
+      .then((element) => {
+        const amount = element[0].childNodes[1].textContent;
+
+        cy.findAllByTestId('table-unit').first().contains('Delete').click();
+        cy.findByRole('button', { name: 'Cancel' }).click();
+        cy.wait(3000);
+        cy.findAllByTestId('table-unit').first().contains(`${amount}`);
+      });
   });
 });
